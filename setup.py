1cb726e3ec026ae32c4ddf57a8867afbaf13bac2
Jon Wayne Parrott <jjramone13@gmail.com>
2014-09-06 16:46:47 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
 
-version = '0.0.3'
+version = '0.0.4'
 
 install_requires = [
     'gevent>=1.0.1',
